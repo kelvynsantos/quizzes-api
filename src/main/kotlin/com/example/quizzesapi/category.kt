b0924389b54c22dd57013a69cb28b180c3d5bb01
Kelvyn Santos <kelvynsantos10@outlook.com>
2021-12-01 18:35:06 -0300

    Criação do controller de categorias

--- src/main/kotlin/com/example/quizzesapi/category.kt
+++ src/main/kotlin/com/example/quizzesapi/category.kt
@@ -1,9 +1,14 @@
 package com.example.quizzesapi
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 import java.util.*
+import javax.validation.Valid
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 data class Category (
     val id: UUID = UUID.randomUUID(),
@@ -11,10 +16,52 @@ data class Category (
     )
 
 val categories  = mutableListOf<Category>()
-
+data class  CategoryReq(
+    @field:[NotNull NotBlank Size(min = 3, max = 20)]
+    val name: String,
+     )
 @RestController
 @RequestMapping("categories")
 class CategoryController {
     @GetMapping
-    fun index() = categories
+    fun index() = ResponseEntity.ok(categories)
+
+    @PostMapping
+    fun create(@Valid @RequestBody categoryReq: CategoryReq): ResponseEntity<Category> {
+        if(categories.any{it.name == categoryReq.name})
+            throw ResponseStatusException(HttpStatus.CONFLICT,"Category alredy exists")
+        val category = Category(name = categoryReq.name)
+        categories.add(category)
+        return ResponseEntity(category, HttpStatus.CREATED)
+    }
+
+    @GetMapping("{id}")
+    fun show(@PathVariable id: UUID): ResponseEntity<Category> {
+        val category = getCategory(id)
+        return ResponseEntity.ok(category)
+    }
+
+    @PutMapping("{id}")
+    fun update(@PathVariable id: UUID, @RequestBody categoryReq: CategoryReq): ResponseEntity<Category> {
+//        val category = categories.first {it.id == id}
+//        category.name = categoryReq.name
+//        return category
+        val category = getCategory(id)
+//        val updatedCategory = Category(category.id, category.name)
+        val updatedCategory = category.copy(name = categoryReq.name)
+        categories.remove(category)
+        categories.add(updatedCategory)
+        return ResponseEntity.ok(updatedCategory)
+    }
+
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: UUID): ResponseEntity<Category> {
+        val category = getCategory(id)
+        categories.remove(category)
+        return ResponseEntity.noContent().build()
+    }
+    private fun getCategory (id: UUID): Category {
+        return categories.firstOrNull {it.id == id} ?:
+        throw ResponseStatusException(HttpStatus.NOT_FOUND,"Category not found")
+    }
 }
---
+++ src/main/kotlin/com/example/quizzesapi/exceptions.kt
@@ -0,0 +1,18 @@
+package com.example.quizzesapi
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+data class FieldError(val name: String, val message: String)
+
+@ControllerAdvice
+class GlobalExceptionHandle {
+
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<List<FieldError>> {
+        return ResponseEntity(ex.fieldErrors.map { FieldError(it.field,it.defaultMessage?:"") }, HttpStatus.BAD_REQUEST)
+    }
+}
